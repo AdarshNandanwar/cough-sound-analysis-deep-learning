db38433097a7f0e441a7165ccf831fa28d26712b
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-06-12 23:22:30 +0530

    fix path

--- download_yt_dataset.py
+++ download_yt_dataset.py
@@ -54,7 +54,7 @@ def download_youtube_audio(label, link, start_time=0.0, duration = 5.0):
 def clear_yt_dataset():
     labels = ['dry', 'wet', 'whooping', 'croup']
     for label in labels:
-        mydir = os.path.join('yt_dataset', label)
+        mydir = os.path.join(BASE_DIR, 'yt_dataset', label)
         if not os.path.exists(mydir):
             os.makedirs(mydir)
         filelist = [ f for f in os.listdir(mydir) if f.endswith(".wav") ]
@@ -62,7 +62,7 @@ def clear_yt_dataset():
             os.remove(os.path.join(mydir, f))
 
 def clear_yt_downloads():
-    mydir = 'yt_downloads'
+    mydir = os.path.join(BASE_DIR, 'yt_downloads')
     if not os.path.exists(mydir):
         os.makedirs(mydir)
     filelist = [ f for f in os.listdir(mydir) if f.endswith(".mp4") ]
@@ -71,7 +71,7 @@ def clear_yt_downloads():
 
     # clear JSON map file
     link_filename_map = {}
-    with open('link_filename_map.json', 'w') as fp:
+    with open(os.path.join(BASE_DIR, 'link_filename_map.json'), 'w') as fp:
         json.dump(link_filename_map, fp)
 
 
