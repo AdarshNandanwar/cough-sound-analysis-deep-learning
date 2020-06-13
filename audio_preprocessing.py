2bae16cb43408871452f68cce4311ce83ad11ad1
Adarsh Nandanwar <nandanwar.adarsh@gmail.com>
2020-06-14 00:26:20 +0530

    add split audio feature, add custom classification dataset

--- audio_preprocessing.py
+++ audio_preprocessing.py
@@ -8,16 +8,16 @@ import librosa
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DETECTION_DATASET_PATH = os.path.join(BASE_DIR, "detection_dataset")
+DETECTION_DATASET_PATHS = [os.path.join(BASE_DIR, "detection_dataset")]
 DETECTION_JSON_PATH = "detection_data.json"
 DETECTION_CSV_PATH = "detection_data.csv"
-CLASSIFICATION_DATASET_PATH = os.path.join(BASE_DIR, "yt_dataset")
+CLASSIFICATION_DATASET_PATHS = [os.path.join(BASE_DIR, "yt_dataset"), os.path.join(BASE_DIR, "classification_dataset")]
 CLASSIFICATION_JSON_PATH = "classification_data.json"
 CLASSIFICATION_CSV_PATH = "classification_data.csv"
 SAMPLE_RATE = 22050
 
 
-def save_features_in_JSON(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+def save_features_in_JSON(dataset_paths, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
     """Extracts MFCCs from dataset and saves them into a json file along with labels.
 
         :param dataset_path (str): Path to dataset
@@ -43,87 +43,89 @@ def save_features_in_JSON(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_
         "chroma_features": [],
     }
 
-    # loop through all sub-folder
-    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+    # loop through all the dataset paths
+    for dataset_path in dataset_paths:
+        # loop through all sub-folder
+        for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
 
-        # ensure we're processing a sub-folder level
-        if dirpath is not dataset_path:
+            # ensure we're processing a sub-folder level
+            if dirpath is not dataset_path:
 
-            # save label (i.e., sub-folder name) in the mapping
-            semantic_label = dirpath.split("/")[-1]
-            data["mapping"].append(semantic_label)
-            log_file.write("\nProcessing: {}\n".format(semantic_label))
+                # save label (i.e., sub-folder name) in the mapping
+                semantic_label = dirpath.split("/")[-1]
+                data["mapping"].append(semantic_label)
+                log_file.write("\nProcessing: {}\n".format(semantic_label))
 
-            # process all audio files in sub-dir
-            for f in filenames:
+                # process all audio files in sub-dir
+                for f in filenames:
 
-                # audio file
-                file_path = os.path.join(dirpath, f)
-                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
-                log_file.write("\nSignal shape: {}, Sample rate: {}".format(signal.shape, sample_rate))
-                
-                track_duration = librosa.get_duration(y=signal, sr=sample_rate)
-                samples_per_track = sample_rate * track_duration
-                log_file.write("\nTrack Duration: {}s, Samples per track: {}".format(track_duration, samples_per_track))
-                
-                samples_per_segment = int(samples_per_track / num_segments)
-                num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
-                log_file.write("\nSamples per segment: {}, No. of mfcc vectors per segment: {}".format(samples_per_segment, num_mfcc_vectors_per_segment))
+                    # audio file
+                    file_path = os.path.join(dirpath, f)
+                    signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+                    log_file.write("\nSignal shape: {}, Sample rate: {}".format(signal.shape, sample_rate))
+                    
+                    track_duration = librosa.get_duration(y=signal, sr=sample_rate)
+                    samples_per_track = sample_rate * track_duration
+                    log_file.write("\nTrack Duration: {}s, Samples per track: {}".format(track_duration, samples_per_track))
+                    
+                    samples_per_segment = int(samples_per_track / num_segments)
+                    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+                    log_file.write("\nSamples per segment: {}, No. of mfcc vectors per segment: {}".format(samples_per_segment, num_mfcc_vectors_per_segment))
 
-                # process all segments of file
-                for d in range(num_segments):
-                    log_file.write("\n{}, segment:{}".format(file_path, d+1))
+                    # process all segments of file
+                    for d in range(num_segments):
+                        log_file.write("\n{}, segment:{}".format(file_path, d+1))
 
-                    # calculate start and finish sample for current segment
-                    start = samples_per_segment * d
-                    finish = start + samples_per_segment
+                        # calculate start and finish sample for current segment
+                        start = samples_per_segment * d
+                        finish = start + samples_per_segment
 
-                    # extract mfcc
-                    mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
-                    mfcc = mfcc.T
-                    log_file.write("\nShape of mfcc: {}".format(mfcc.shape))
-                    # store only mfcc feature with expected number of vectors
-                    if len(mfcc) == num_mfcc_vectors_per_segment:
-                        data["mfccs"].append(mfcc.tolist())
-                        data["labels"].append(i-1) 
+                        # extract mfcc
+                        mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                        mfcc = mfcc.T
+                        log_file.write("\nShape of mfcc: {}".format(mfcc.shape))
+                        # store only mfcc feature with expected number of vectors
+                        if len(mfcc) == num_mfcc_vectors_per_segment:
+                            data["mfccs"].append(mfcc.tolist())
+                            data["labels"].append(i-1) 
+                            
+                        ##################################
+                        # ADD IF CHECKS ON EVERY FEATURE #  
+                        ##################################
                         
-                    ##################################
-                    # ADD IF CHECKS ON EVERY FEATURE #  
-                    ##################################
-                    
-                    # extract spectral centeroid
-                    spectral_centroids = librosa.feature.spectral_centroid(signal[start:finish], sr=sample_rate)[0]
-                    log_file.write("\nShape of spectral centroids: {}".format(spectral_centroids.shape))
-                    data["spectral_centroids"].append(spectral_centroids.tolist())
-                    
-                    # extract spectral rolloff
-                    spectral_rolloffs = librosa.feature.spectral_rolloff(signal[start:finish]+0.01, sr=sample_rate)[0]
-                    log_file.write("\nShape of spectral rolloffs: {}".format(spectral_rolloffs.shape))
-                    data["spectral_rolloffs"].append(spectral_rolloffs.tolist())
-                    
-                    # extract spectral bandwidth
-                    spectral_bandwidth_2 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate)[0]
-                    log_file.write("\nShape of spectral bandwidth 2: {}".format(spectral_bandwidth_2.shape))
-                    data["spectral_bandwidth_2"].append(spectral_bandwidth_2.tolist())
-                    spectral_bandwidth_3 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate, p=3)[0]
-                    log_file.write("\nShape of spectral bandwidth 3: {}".format(spectral_bandwidth_3.shape))
-                    data["spectral_bandwidth_3"].append(spectral_bandwidth_3.tolist())
-                    spectral_bandwidth_4 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate, p=4)[0]
-                    log_file.write("\nShape of spectral bandwidth 4: {}".format(spectral_bandwidth_4.shape))
-                    data["spectral_bandwidth_4"].append(spectral_bandwidth_4.tolist())
-                    
-                    # extract zero-crossing rate                    
-                    zero_crossing_rates = librosa.feature.zero_crossing_rate(signal[start:finish], pad=False)[0]
-                    log_file.write("\nShape of zero crossing rates: {}".format(zero_crossing_rates.shape))
-                    data["zero_crossing_rates"].append(zero_crossing_rates.tolist())
-                    
-                    # extract croma features
-                    chroma_features = librosa.feature.chroma_stft(signal[start:finish], sr=sample_rate, hop_length=hop_length)
-                    chroma_features = chroma_features.T
-                    log_file.write("\nShape of chroma features: {}".format(chroma_features.shape))
-                    data["chroma_features"].append(chroma_features.tolist())
-                    
-                log_file.write("\n")
+                        # extract spectral centeroid
+                        spectral_centroids = librosa.feature.spectral_centroid(signal[start:finish], sr=sample_rate)[0]
+                        log_file.write("\nShape of spectral centroids: {}".format(spectral_centroids.shape))
+                        data["spectral_centroids"].append(spectral_centroids.tolist())
+                        
+                        # extract spectral rolloff
+                        spectral_rolloffs = librosa.feature.spectral_rolloff(signal[start:finish]+0.01, sr=sample_rate)[0]
+                        log_file.write("\nShape of spectral rolloffs: {}".format(spectral_rolloffs.shape))
+                        data["spectral_rolloffs"].append(spectral_rolloffs.tolist())
+                        
+                        # extract spectral bandwidth
+                        spectral_bandwidth_2 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate)[0]
+                        log_file.write("\nShape of spectral bandwidth 2: {}".format(spectral_bandwidth_2.shape))
+                        data["spectral_bandwidth_2"].append(spectral_bandwidth_2.tolist())
+                        spectral_bandwidth_3 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate, p=3)[0]
+                        log_file.write("\nShape of spectral bandwidth 3: {}".format(spectral_bandwidth_3.shape))
+                        data["spectral_bandwidth_3"].append(spectral_bandwidth_3.tolist())
+                        spectral_bandwidth_4 = librosa.feature.spectral_bandwidth(signal[start:finish]+0.01, sr=sample_rate, p=4)[0]
+                        log_file.write("\nShape of spectral bandwidth 4: {}".format(spectral_bandwidth_4.shape))
+                        data["spectral_bandwidth_4"].append(spectral_bandwidth_4.tolist())
+                        
+                        # extract zero-crossing rate                    
+                        zero_crossing_rates = librosa.feature.zero_crossing_rate(signal[start:finish], pad=False)[0]
+                        log_file.write("\nShape of zero crossing rates: {}".format(zero_crossing_rates.shape))
+                        data["zero_crossing_rates"].append(zero_crossing_rates.tolist())
+                        
+                        # extract croma features
+                        chroma_features = librosa.feature.chroma_stft(signal[start:finish], sr=sample_rate, hop_length=hop_length)
+                        chroma_features = chroma_features.T
+                        log_file.write("\nShape of chroma features: {}".format(chroma_features.shape))
+                        data["chroma_features"].append(chroma_features.tolist())
+                        
+                    log_file.write("\n")
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
@@ -165,7 +167,7 @@ def get_features_csv_row(signal, sample_rate, num_mfcc=13, n_fft=2048, hop_lengt
 
     return csv_row
         
-def save_features_in_CSV(dataset_path, csv_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+def save_features_in_CSV(dataset_paths, csv_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
     """Extracts MFCCs from dataset and saves them into a csv file along with labels.
 
         :param dataset_path (str): Path to dataset
@@ -179,42 +181,44 @@ def save_features_in_CSV(dataset_path, csv_path, num_mfcc=13, n_fft=2048, hop_le
 
     with open(csv_path, 'w', newline='') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        # loop through all sub-folder
-        for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
-            # ensure we're processing a sub-folder level
-            if dirpath is not dataset_path:
-                # save label  mapping
-                semantic_label = dirpath.split("/")[-1]
-                mapping_file.write("\n{} : {}".format(i-1, semantic_label))
-                # process all audio files in sub-dir
-                for f in filenames:
-                    # audio file
-                    file_path = os.path.join(dirpath, f)
-                    signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
-                    log_file.write("\nSignal shape: {}, Sample rate: {}".format(signal.shape, sample_rate))
-                    
-                    track_duration = librosa.get_duration(y=signal, sr=sample_rate)
-                    samples_per_track = sample_rate * track_duration
-                    log_file.write("\nTrack Duration: {}s, Samples per track: {}".format(track_duration, samples_per_track))
-                    
-                    samples_per_segment = int(samples_per_track / num_segments)
-                    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
-                    log_file.write("\nSamples per segment: {}, No. of mfcc vectors per segment: {}".format(samples_per_segment, num_mfcc_vectors_per_segment))
-
-                    # process all segments of file
-                    for d in range(num_segments):
-                        log_file.write("\n{}, segment:{}".format(file_path, d+1))
-
-                        # calculate start and finish sample for current segment
-                        start = samples_per_segment * d
-                        finish = start + samples_per_segment
-
-                        csv_row = get_features_csv_row(signal[start:finish], sample_rate, num_mfcc, n_fft, hop_length)
-                        # label
-                        csv_row += [i-1]
-                        wr.writerow(csv_row)
+        # loop through all the dataset paths
+        for dataset_path in dataset_paths:
+            # loop through all sub-folder
+            for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+                # ensure we're processing a sub-folder level
+                if dirpath is not dataset_path:
+                    # save label  mapping
+                    semantic_label = dirpath.split("/")[-1]
+                    mapping_file.write("\n{} : {}".format(i-1, semantic_label))
+                    # process all audio files in sub-dir
+                    for f in filenames:
+                        # audio file
+                        file_path = os.path.join(dirpath, f)
+                        signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+                        log_file.write("\nSignal shape: {}, Sample rate: {}".format(signal.shape, sample_rate))
                         
-                    log_file.write("\n")
+                        track_duration = librosa.get_duration(y=signal, sr=sample_rate)
+                        samples_per_track = sample_rate * track_duration
+                        log_file.write("\nTrack Duration: {}s, Samples per track: {}".format(track_duration, samples_per_track))
+                        
+                        samples_per_segment = int(samples_per_track / num_segments)
+                        num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+                        log_file.write("\nSamples per segment: {}, No. of mfcc vectors per segment: {}".format(samples_per_segment, num_mfcc_vectors_per_segment))
+
+                        # process all segments of file
+                        for d in range(num_segments):
+                            log_file.write("\n{}, segment:{}".format(file_path, d+1))
+
+                            # calculate start and finish sample for current segment
+                            start = samples_per_segment * d
+                            finish = start + samples_per_segment
+
+                            csv_row = get_features_csv_row(signal[start:finish], sample_rate, num_mfcc, n_fft, hop_length)
+                            # label
+                            csv_row += [i-1]
+                            wr.writerow(csv_row)
+                            
+                        log_file.write("\n")
         
 if __name__ == "__main__":
 
@@ -223,20 +227,20 @@ if __name__ == "__main__":
     print("Creating detection dataset.")
     mapping_file.write("\nDetection:")
     # log_file = open(os.path.join(BASE_DIR, "detection_log.txt"), 'w')
-    # save_features_in_JSON(DETECTION_DATASET_PATH, DETECTION_JSON_PATH, num_segments=1)
+    # save_features_in_JSON(DETECTION_DATASET_PATHS, DETECTION_JSON_PATH, num_segments=1)
     # log_file.close()
     log_file = open(os.path.join(BASE_DIR, "detection_log.txt"), 'w')
-    save_features_in_CSV(DETECTION_DATASET_PATH, DETECTION_CSV_PATH, num_segments=1)
+    save_features_in_CSV(DETECTION_DATASET_PATHS, DETECTION_CSV_PATH, num_segments=1)
     log_file.close()
     print("Detection dataset created successuflly.")
 
     print("Creating classification dataset.")
-    mapping_file.write("\Classification:")
+    mapping_file.write("\nClassification:")
     # log_file = open(os.path.join(BASE_DIR, "classification_log.txt"), 'w')
-    # save_features_in_JSON(CLASSIFICATION_DATASET_PATH, CLASSIFICATION_JSON_PATH, num_segments=1)
+    # save_features_in_JSON(CLASSIFICATION_DATASET_PATHS, CLASSIFICATION_JSON_PATH, num_segments=1)
     # log_file.close()
     log_file = open(os.path.join(BASE_DIR, "classification_log.txt"), 'w')
-    save_features_in_CSV(CLASSIFICATION_DATASET_PATH, CLASSIFICATION_CSV_PATH, num_segments=1)
+    save_features_in_CSV(CLASSIFICATION_DATASET_PATHS, CLASSIFICATION_CSV_PATH, num_segments=1)
     log_file.close()
     print("Classification dataset created successuflly.")
     mapping_file.close()
--- download_yt_dataset.py
+++ download_yt_dataset.py
@@ -3,12 +3,14 @@
 from pytube import YouTube
 import soundfile as sf
 import librosa
+import numpy as np
 import json
 import os
 import requests
 from clear_yt_dataset import clear_yt_dataset, clear_yt_downloads
 
 SAMPLE_RATE = 22050
+DATASET_AUDIO_DURATION = 5
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 link_filename_map = {}
 curr_file_number = 0
@@ -41,12 +43,42 @@ def download_youtube_audio(label, link, start_time=0.0, duration = 5.0):
 
         # crop and save audio file
         file_path = os.path.join(BASE_DIR, download_path, filename+'.mp4')
-        signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE, offset=start_time, duration=duration)
         cur_dir = os.path.join(BASE_DIR, 'yt_dataset', label)
         if not os.path.exists(cur_dir):
             os.makedirs(cur_dir)
-        sf.write(os.path.join(cur_dir, str(curr_file_number)+'.wav'), signal, SAMPLE_RATE)
-        curr_file_number += 1
+
+        if start_time is None:
+            min_duration = 1.5
+            max_duration = DATASET_AUDIO_DURATION
+            threshold = 67
+            signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+            cough_intevals = librosa.effects.split(signal, top_db=threshold)
+            for interval in cough_intevals:
+                int_st = interval[0]
+                int_en = interval[1]
+                step = sample_rate*max_duration
+                for i in range(int_st, int_en, step):
+                    if int_en-i >= sample_rate*min_duration:
+                        fixed_signal = signal[i:min(i+step, int_en)]
+                        if int_en-i < step:
+                            fixed_signal = librosa.util.fix_length(fixed_signal, step)
+                        sf.write(os.path.join(cur_dir, str(curr_file_number)+'.wav'), fixed_signal, SAMPLE_RATE)
+                        curr_file_number += 1
+
+        else:
+            signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE, offset=start_time, duration=duration)
+            int_st = np.int64(0)
+            int_en = np.int64(duration*sample_rate)
+            min_duration = 1.5
+            max_duration = DATASET_AUDIO_DURATION
+            step = sample_rate*max_duration
+            for i in range(int_st, int_en, step):
+                if int_en-i >= sample_rate*min_duration:
+                    fixed_signal = signal[i:min(i+step, int_en)]
+                    if int_en-i < step:
+                        fixed_signal = librosa.util.fix_length(fixed_signal, step)
+                    sf.write(os.path.join(cur_dir, str(curr_file_number)+'.wav'), fixed_signal, SAMPLE_RATE)
+                    curr_file_number += 1
 
     except Exception as e:
         print(e)
@@ -65,7 +97,15 @@ if __name__ == "__main__":
         google_sheets_data = google_sheets_data.json()['feed'].get('entry')
         if google_sheets_data:
             for entry in google_sheets_data:
-                download_youtube_audio(label=labels[i], link=entry['gsx$link']['$t'], start_time=float(entry['gsx$starttime']['$t']), duration=float(entry['gsx$duration']['$t']))
+                try:
+                    start_time = float(entry['gsx$starttime']['$t'])
+                    duration = float(entry['gsx$duration']['$t'])
+                except Exception as e:
+                    start_time = None
+                    duration = None
+                
+                print(labels[i], entry['gsx$link']['$t'], start_time, duration)
+                download_youtube_audio(label=labels[i], link=entry['gsx$link']['$t'], start_time=start_time, duration=duration)
 
     with open(os.path.join(BASE_DIR, 'link_filename_map.json'), 'w') as fp:
         json.dump(link_filename_map, fp)
